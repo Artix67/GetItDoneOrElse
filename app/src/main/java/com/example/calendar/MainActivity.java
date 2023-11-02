7049620ceeb2f2760e5cdbaec585ea7ce96af86d
KevinLorne <116528149+KevinLorne@users.noreply.github.com>
2023-11-02 12:00:42 -0400

    Calendar
    is working need to do more editing to make it have our look for our application works

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  CalendarAdapter.OnItemListener {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectDate;
@@ -28,7 +34,68 @@ public class MainActivity extends AppCompatActivity {
         monthYearText = findViewById(R.id.MonthYearTV);
     }
 
-    private void setMonthView() {
+    private void setMonthView()
+    {
+     monthYearText.setText(monthYearFromDate(selectDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth,this );
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+
+    private ArrayList<String> daysInMonthArray(LocalDate Date)
+    {
+        ArrayList<String> dayInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(Date);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        LocalDate firstOfMonth = selectDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for (int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                dayInMonthArray.add("");
+            }
+            else
+            {
+                dayInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+
+        }
+        return  dayInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public  void previousMonthAction(View view)
+
+    {
+        selectDate = selectDate.minusMonths(1);
+          setMonthView();
+    }
+
+    public void nextMonthAction(View view)
+    {
+      selectDate = selectDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText)
+    {
+        if(dayText.equals(""))
+        {
+            String message = selectDate + dayText + " " + monthYearFromDate(selectDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
 
     }
 }
