d3c61d3eafa124170fc23c2d0e883bcf311cec61
AliyahEM <124080491+AliyahEM@users.noreply.github.com>
2023-11-09 02:30:21 -0500

    Sign Up UI Complete. Will start on Login page....

--- app/src/main/java/com/example/login20/RegistrationScreen.java
+++ app/src/main/java/com/example/login20/RegistrationScreen.java
@@ -2,7 +2,9 @@ package com.example.login20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class RegistrationScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+    }
+
+    public void signin(View view) {
+        startActivity(new Intent(RegistrationScreen.this,LoginScreen.class));
+
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(RegistrationScreen.this,MainActivity.class));
     }
 }
