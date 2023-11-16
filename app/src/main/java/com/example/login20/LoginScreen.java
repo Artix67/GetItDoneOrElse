b9bd89a35e93f180adb92ac3b8c84faacaad6425
AliyahEM <124080491+AliyahEM@users.noreply.github.com>
2023-11-16 00:26:55 -0500

    Sign In almost Completed. Will start on Functions after implementing User and Password Icons....

--- app/src/main/java/com/example/login20/LoginScreen.java
+++ app/src/main/java/com/example/login20/LoginScreen.java
@@ -2,7 +2,9 @@ package com.example.login20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+
     }
+
+    public void signIn(View view) {
+        startActivity(new Intent(LoginScreen.this,MainActivity.class));
+
+    }
+
+    public void signUp(View view) {
+        startActivity(new Intent(LoginScreen.this,RegistrationScreen.class));
+
+    }
+
 }
--- app/src/main/java/com/example/login20/RegistrationScreen.java
+++ app/src/main/java/com/example/login20/RegistrationScreen.java
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class RegistrationScreen extends AppCompatActivity {
+
+    EditText name, email, username, password, reEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+
+        getSupportActionBar().hide();
+
+        name = findViewById(R.id.editText4);
+        email = findViewById(R.id.editText3);
+        username = findViewById(R.id.editText7);
+        password = findViewById(R.id.editText5);
+        reEnter = findViewById(R.id.editText8);
+
     }
 
     public void signin(View view) {
@@ -20,6 +36,60 @@ public class RegistrationScreen extends AppCompatActivity {
     }
 
     public void signup(View view) {
+
+        String nameuser = name.getText().toString();
+        String userName = username.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+        String userReEnter = reEnter.getText().toString();
+
+        if (TextUtils.isEmpty(nameuser)){
+
+            Toast.makeText(this, "Name", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if (TextUtils.isEmpty(userName)){
+
+            Toast.makeText(this, "Username", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if (TextUtils.isEmpty(userEmail)){
+
+            Toast.makeText(this, "Email", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if (TextUtils.isEmpty(userPassword)){
+
+            Toast.makeText(this, "Password", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        if (TextUtils.isEmpty(userReEnter)){
+
+            Toast.makeText(this, "Repeat Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length() < 6){
+
+            Toast.makeText(this, "Password too short, minimum of 6 characters. ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userReEnter.matches(userPassword))
+        {
+            Toast.makeText(this, "Passwords match!", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            Toast.makeText(this, "Passwords needs to match.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
         startActivity(new Intent(RegistrationScreen.this,MainActivity.class));
+
+
     }
 }
