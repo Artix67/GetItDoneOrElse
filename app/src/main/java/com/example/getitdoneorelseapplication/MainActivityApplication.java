61302fcc373acc5415c15b03efc59c699b1d4f57
KevinLorne <116528149+KevinLorne@users.noreply.github.com>
2023-11-18 12:38:22 -0500

    set up for main application

---
+++ app/src/main/java/com/example/getitdoneorelseapplication/MainActivityApplication.java
@@ -0,0 +1,49 @@
+package com.example.getitdoneorelseapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivityApplication extends AppCompatActivity implements View.OnClickListener {
+    private Button btn;
+    private Button btn1;
+    private  Button btn2;
+    private Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+        initWidgets();
+
+        btn.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+
+    private void initWidgets() {
+
+        btn = findViewById(R.id.buttonKevin);
+        btn2 =  findViewById(R.id.buttonMiki);
+        btn3 = findViewById(R.id.buttonNatalie);
+        btn1 = findViewById(R.id.buttonArtix);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonKevin) {
+            // Your code for buttonKevin click
+        } else if (view.getId() == R.id.buttonMiki) {
+            // Your code for buttonMiki click
+        } else if (view.getId() == R.id.buttonNatalie) {
+            // Your code for buttonNatalie click
+        } else if (view.getId() == R.id.buttonArtix) {
+            // Your code for buttonArtix click
+        }
+    }
+}
