3bf787900e6d1096aacd4d8c53965d745fd84add
Robert <118757168+Artix67@users.noreply.github.com>
2023-12-07 12:32:44 -0600

    cleaned up some errors
    cleaned up some errors

--- app/src/main/java/com/example/getitdoneorelseapplication/artix/CalendarView.java
+++ app/src/main/java/com/example/getitdoneorelseapplication/artix/CalendarView.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.getitdoneorelseapplication.calendar.MainActivityCalendar;
+
 public class CalendarView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home.xml); // Replace with your layout file
+        setContentView(R.layout.fragment_home.xml);
 
         android.widget.CalendarView calendarView = findViewById(R.id.calendarView);
 
