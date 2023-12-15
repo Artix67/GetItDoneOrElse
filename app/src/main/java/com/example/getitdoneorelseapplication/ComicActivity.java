11d617f52fa8f1243628d41f551b42bb81622ff8
nlopez5 <nlopez1685@gmail.com>
2023-12-15 10:46:43 -0600

    updated
    updates

--- app/src/main/java/com/example/getitdoneorelseapplication/ComicActivity.java
+++ app/src/main/java/com/example/getitdoneorelseapplication/ComicActivity.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.getitdoneorelseapplication.settings.CharacterSelection;
+
 public class ComicActivity extends AppCompatActivity {
 
     @Override
@@ -18,33 +20,47 @@ public class ComicActivity extends AppCompatActivity {
         ImageView alexTwoImageView = findViewById(R.id.comic_alex_two);
         ImageView alexThreeImageView = findViewById(R.id.comic_alex_three);
 
-        alexOneImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the click event for AlexOneImageView
-                launchPopUpActivity(ComicAlexOneActivity.class);
-            }
+        alexOneImageView.setOnClickListener(v -> {
+            // Handle the click event for AlexOneImageView
+            launchPopUpActivity(R.layout.comic_alex_one);
         });
 
-        alexTwoImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the click event for AlexTwoImageView
-                launchPopUpActivity(ComicAlexTwoActivity.class);
-            }
+        alexTwoImageView.setOnClickListener(v -> {
+            // Handle the click event for AlexTwoImageView
+            launchPopUpActivity(R.layout.comic_alex_two);
         });
 
-        alexThreeImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the click event for AlexThreeImageView
-                launchPopUpActivity(ComicAlexThreeActivity.class);
-            }
+        alexThreeImageView.setOnClickListener(v -> {
+            // Handle the click event for AlexThreeImageView
+            launchPopUpActivity(R.layout.comic_alex_three);
         });
     }
 
-    private void launchPopUpActivity(Class<?> popUpActivityClass) {
-        Intent intent = new Intent(this, popUpActivityClass);
+    private void launchPopUpActivity(int layoutResourceId) {
+        Intent intent = new Intent(this, CharacterSelection.class);
+        intent.putExtra("layoutResourceId", layoutResourceId);
         startActivity(intent);
     }
+
+    public void onImageClicked(View view) {
+        int imageId = view.getId();
+
+        Intent intent = new Intent(this, CharacterSelection.class);
+
+        // Determine which image was clicked based on its ID
+        int layoutResourceId;
+        if (imageId == R.id.comic_alex_one) {
+            layoutResourceId = R.layout.comic_alex_one;
+        } else if (imageId == R.id.comic_alex_two) {
+            layoutResourceId = R.layout.comic_alex_two;
+        } else if (imageId == R.id.comic_alex_three) {
+            layoutResourceId = R.layout.comic_alex_three;
+        } else {
+            return;
+        }
+
+        intent.putExtra("layoutResourceId", layoutResourceId);
+        startActivity(intent);
+    }
+
 }
